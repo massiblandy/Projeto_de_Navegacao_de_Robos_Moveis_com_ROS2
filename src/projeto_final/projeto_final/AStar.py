aeeb2d5d5095d1859793a679f80fcad96dfaac32
Massiel Blandy Ramón <massi00br@gmail.com>
2024-06-02 17:57:13 +0000

    Versao final do projeto

--- src/projeto_final/projeto_final/AStar.py
+++ src/projeto_final/projeto_final/AStar.py
@@ -199,7 +199,7 @@ class PathNode(Node):
         self.get_logger().info('Expanding obstacles in the map')
         self.get_logger().info('It could take some time. Please wait!!!')
         #Expande os obstáculos na matriz do mapa para garantir uma distância segura
-        matrix = expand_obstacles(matrix, expansion_size=11) #Mudar este valor para o valor desejado em dependência da disposição dos obstáculos e do caminho
+        matrix = expand_obstacles(matrix, expansion_size=12) #Mudar este valor para o valor desejado em dependência da disposição dos obstáculos e do caminho
 
         self.plot_map(matrix) #Plota a matriz do mapa expandido
 
@@ -210,10 +210,8 @@ class PathNode(Node):
         self.get_logger().info(f'Starting A* from: {start} (Gazebo: {start_x}, {start_y})')
 
         #Coordenadas de/ destino no mapa
-        end_x = 0
-        end_y = 0
-        #end_x = -8.5
-        #end_y = -5.5
+        end_x = -8
+        end_y = -5.5
         end = self.convert_coordinates(end_x, end_y)
         self.get_logger().info(f'Ending A* at: {end} (Gazebo: {end_x}, {end_y})')
 
